30e15e6e11a0ca2ca8e7b862a7df064a5b6291c8
ozankiratli <ozankiratli@gmail.com>
2016-03-28 11:51:47 -0400

    Added files via upload

---
+++ variables.h
@@ -0,0 +1,36 @@
+/*
+ * variables.h
+ *
+ *  Created on: Mar 15, 2016
+ *      Author: ozan
+ */
+
+#ifndef VARIABLES_H_
+#define VARIABLES_H_
+
+extern const int t_bot;
+extern const int t_growth;
+
+extern const long int ngenes;
+extern const long int l;
+extern const int nmutator;
+
+extern double mu_d;
+extern double mu_b;
+extern double mu_n;
+extern double inc_mut_rate;
+extern double mean_e_mutator;
+
+extern double shaped ;
+extern double scaled;
+extern double betab;
+
+extern double selecthighest;
+
+extern bool mutator_control;
+extern bool increased_mutator;
+extern bool del_dep;
+
+extern int run;
+
+#endif /* VARIABLES_H_ */
