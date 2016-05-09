b683b990267279e8d9fe7223023ad55d284a596e
ozankiratli <ozankiratli@gmail.com>
2016-05-09 18:41:14 -0400

    Update variables.h

--- variables.h
+++ variables.h
@@ -1,37 +1,34 @@
-/*
- * variables.h
- *
- *  Created on: Mar 15, 2016
- *      Author: ozan
- */
-
 #ifndef __VARIABLES_H_
 #define __VARIABLES_H_
-
-
-#define T_bot 					50
-#define T_growth		 		25
-
-#define Ngenes 					4400
-#define L 						4600000
-#define Nmutator				20
-
-#define Mu_d					0.9
-#define Mu_b					0.001
-#define Mu_n 					9.9
-#define Inc_mut_rate			0.1
-#define Mean_e_mutator			0.05
-
-#define Shaped 					0.1
-#define Scaled					0.3
-#define Betab					0.01
-
-#define Selecthighest 			0.4
-
-#define Mutator_control 		1
-#define Increased_mutator		1
-#define Del_dep					1
-
-#define Run						0
-
-#endif /* VARIABLES_H_ */
+ 
+ 
+#define T_bot            50
+#define T_growth         25
+#define N_growth         1000000
+ 
+#define Ngenes           4400
+#define L                4640000
+#define Nmutator         20
+ 
+#define Mu_d             0.9
+#define Mu_b             0.01
+#define Mu_n             9.9
+#define Mean_e_mutator   0.9
+#define Var_e_mutator    0.2
+#define WtoOffa          -0.26
+#define WtoOffb          8.54
+#define WtoOffc          -10.26
+#define WtoOffd          4.34
+#define W_sel            0.25
+ 
+#define Shaped           0.3
+#define Scaled           0.1
+#define Betab            0.01
+ 
+#define Selecthighest    0.5
+ 
+#define Mutator_control      1
+ 
+#define Run               0
+ 
+#endif
